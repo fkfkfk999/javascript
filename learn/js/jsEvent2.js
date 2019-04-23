130d2bc5794015eb969d7e4cae2303998a444447
fkfkfk999 <fkfkfk9@daum.net>
2019-04-23 14:08:30 +0900

    JS 이벤트 2

---
+++ learn/js/jsEvent2.js
@@ -0,0 +1,22 @@
+const NORMAR = "normar";
+const MOUSEHOVER = "mouseHover";
+let title = document.querySelector("#title");
+
+// title.addEventListener("mouseenter", function() {
+//   if (title.className == NORMAR) title.className = MOUSEHOVER;
+//   else title.className = NORMAR;
+// });
+
+// title.addEventListener("mouseenter", function() {
+//   if (title.classList.contains(NORMAR)) {
+//     title.classList.remove(NORMAR);
+//     title.classList.add(MOUSEHOVER);
+//   } else {
+//     title.classList.remove(MOUSEHOVER);
+//     title.classList.add(NORMAR);
+//   }
+// });
+
+// title.addEventListener("mouseenter", function() {
+//   title.classList.toggle(MOUSEHOVER);
+// });
