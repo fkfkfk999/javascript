307d14ead1dda3a66809823a9e7f5d63334b2bae
fkfkfk999 <fkfkfk9@daum.net>
2019-04-23 14:16:21 +0900

    JS 이벤트

--- learn/js/jsEvent.js
+++ learn/js/jsEvent.js
@@ -5,12 +5,19 @@
 // window.addEventListener("resize", function() {
 //   alert("사이즈가 변경되었습니다.");
 // });
-let checkboxs = document.querySelector(".numbers");
+let checkboxs = document.querySelectorAll(".numbers");
 let title = document.querySelector("#title");
 const BASE_COLOR = "black";
-checkboxs.addEventListener("click", function(event) {
-  console.log(event);
-  alert(this.value);
+console.log(checkboxs);
+// checkboxs.addEventListener("click", function(event) {
+//   console.log(event);
+//   alert(this.value);
+// });
+Array.from(checkboxs).forEach(checkbox => {
+  checkbox.addEventListener("click", function(event) {
+    console.log(event);
+    alert(this.value);
+  });
 });
 title.addEventListener("mouseover", function() {
   if (title.style.color == BASE_COLOR) title.style.color = "rgb(29, 131, 226)";
