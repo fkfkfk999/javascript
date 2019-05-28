9f21ee3760e3e3fed6ad3b8f1c9c6b0c471d015e
fkfkfk999 <fkfkfk9@daum.net>
2019-05-28 10:28:04 +0900

    Update todo.js

--- learn/finalProject/js/todo.js
+++ learn/finalProject/js/todo.js
@@ -2,18 +2,38 @@ const todoFrm = document.querySelector("#todoFrm"),
   todoInput = todoFrm.querySelector("#todoInput"),
   todoList = document.querySelector(".todoList");
 
-const TodoList_LS = "todo";
+const todoList_LS = "todo",
+  arrTodo = [];
 
+function localSaveTodo() {
+  // 일반 오브젝트를 로컬에 저장시키면 String만 저장되는 특성상 제대로 저장할 수 없다.
+  // 그렇기 때문에 JSON으로 변환하여 저장시킨다.
+  localStorage.setItem(todoList_LS, JSON.stringify(arrTodo));
+}
 function saveTodo(todoVal) {
   console.log(todoVal);
+  // 입력한 할일을 사용자에게 보여준다.
   const todoLi = document.createElement("li");
   const delBtn = document.createElement("button");
-  delBtn.innerText = "❌";
   const todoSpan = document.createElement("span");
+  const seqNo = arrTodo.length + 1;
+  const id = localStorage.getItem(USER_ID);
+
+  todoLi.id = "todoLi" + seqNo;
+  delBtn.innerText = "❌";
   todoSpan.innerText = todoVal;
   todoLi.appendChild(todoSpan);
   todoLi.appendChild(delBtn);
   todoList.appendChild(todoLi);
+
+  //할일을 객체로 만들어 배열의 형태로 저장시킨다.
+  todoObj = {
+    todoStr: todoVal,
+    seqNo: seqNo,
+    id: id
+  };
+  arrTodo.push(todoObj);
+  localSaveTodo();
 }
 // todo의 submit핸들러
 function handleTodoSubmit(event) {
@@ -34,9 +54,18 @@ function handleTodoSubmit(event) {
 }
 // 로컬에 저장된 할일을 불러온다.
 function loadTodoList() {
-  const todo = localStorage.getItem("TodoList_LS");
+  const todo = localStorage.getItem(todoList_LS);
   // 할일이 있을 경우 리스트에 출력 시켜야 한다.
   if (todo !== null) {
+    console.log(todo);
+    //저장한 데이터가 JSON형태이기 때문에 그냥 출력하면 JSON데이터가 출력된다.
+    // 일단 파싱을 해서 객체형태로 바꿔줘야 한다.
+    const jsonParsingTodo = JSON.parse(todo);
+    console.log(jsonParsingTodo);
+    jsonParsingTodo.forEach(function(jpt) {
+      console.log(jpt.todoStr);
+      saveTodo(jpt.todoStr);
+    });
   }
 }
 function init() {
