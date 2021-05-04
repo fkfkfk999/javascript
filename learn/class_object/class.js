d78388a511b055291cd683a3b907b8524bed2e94
fkfkfk9 <yjh88learn@gmail.com>
2021-05-04 23:55:34 +0900

    드림코딩 예제
    내려받기만 완료

---
+++ learn/class_object/class.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function sum(num, num, num2) {
+  return num + num + num2;
+}
+
+console.log(sum(3, 5, 8));
---
+++ learn/class_object/main.js
@@ -0,0 +1,3 @@
+console.log("Hello World");
+const div = document.getElementById("test");
+console.log(div);
